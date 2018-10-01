b2fff33bad405cc6e42012f96c9eb51e573c058e
Akira Ota 10nin <akira19870221@gmail.com>
2018-10-01 20:50:14 +0900

    optimize code, and add print function.

--- cap_reverse.rb
+++ cap_reverse.rb
@@ -20,42 +20,36 @@ class CapRange
   end
 
   def info
-    [from: @from, to: @to, dir: @direction]
+    {from: @from, to: @to, dir: @direction}
   end
 end
 
 def create_cap_list(len)
-  Array.new(len){ Cap.new(rand(0..1)) }
+  Array.new(len){ Cap.new(rand(0..1)) } << nil
 end
 
 def count_caps(cap_list)
-  cap_dir = cap_list[0].dir?
   front = back = start = 0
+  range_list = []
 
-  command_list = []
-  i = 0
-  cap_list.each do |c|
-    if c.dir? != cap_dir
-      command_list << CapRange.new(start, i-1, c.dir?)
-      start = i
+  cap_list.each_with_index do |c, i|
+    if c.nil? || c.dir? != cap_list[start].dir?
+      range_list << CapRange.new(start, i-1, cap_list[start])
 
-      if c.dir? == 1
+      if cap_list[start].dir? == 1
         front += 1
-      else
+      elsif
         back += 1
       end
-    else
-      i += 1
+      start = i
     end
-  end
+  end  
+  {range: range_list, front: front, back: back}
+end
 
-  # for latest elements
-  command_list << CapRange.new(start, i-1, cap_list[-1].dir?)
-  if cap_list[-1].dir? == 1
-    front += 1
-  else
-    back += 1
+def print_cap_list(cap_list)
+  cap_list.each do |x|
+    printf x&.dir?.to_s + ' '
   end
-  
-  [range: command_list, front: front, back: back]
+  puts ''
 end
