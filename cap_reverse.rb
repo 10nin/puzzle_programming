e2db456eb29d62cf8a410260fe12e6247d1cf12c
Akira Ota 10nin <akira19870221@gmail.com>
2018-10-01 19:25:59 +0900

    fix cap list initialize bug.

--- cap_reverse.rb
+++ cap_reverse.rb
@@ -25,6 +25,37 @@ class CapRange
 end
 
 def create_cap_list(len)
-  Array.new(len, Cap.new(rand(0..1)))
+  Array.new(len){ Cap.new(rand(0..1)) }
 end
 
+def count_caps(cap_list)
+  cap_dir = cap_list[0].dir?
+  front = back = start = 0
+
+  command_list = []
+  i = 0
+  cap_list.each do |c|
+    if c.dir? != cap_dir
+      command_list << CapRange.new(start, i-1, c.dir?)
+      start = i
+
+      if c.dir? == 1
+        front += 1
+      else
+        back += 1
+      end
+    else
+      i += 1
+    end
+  end
+
+  # for latest elements
+  command_list << CapRange.new(start, i-1, cap_list[-1].dir?)
+  if cap_list[-1].dir? == 1
+    front += 1
+  else
+    back += 1
+  end
+  
+  [range: command_list, front: front, back: back]
+end
