30a2ff0f2fe2d945212c44530019a32d1c767952
Akira Ota 10nin <akira19870221@gmail.com>
2018-10-01 19:09:04 +0900

    add range class

--- cap_reverse.rb
+++ cap_reverse.rb
@@ -2,6 +2,26 @@ class Cap
   def initialize(dir)
     @direction = dir
   end
+
+  def flip!
+    @direction = @direction ^ 1
+  end
+
+  def dir?
+    @direction
+  end
+end
+
+class CapRange
+  def initialize(from, to, dir)
+    @from = from
+    @to = to
+    @direction = dir
+  end
+
+  def info
+    [from: @from, to: @to, dir: @direction]
+  end
 end
 
 def create_cap_list(len)
