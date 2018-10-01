fb16e25ffc15443d93a161b977dc440dc6ab52ce
Akira Ota 10nin <akira19870221@gmail.com>
2018-10-01 21:28:38 +0900

    Eureka!!

--- cap_reverse.rb
+++ cap_reverse.rb
@@ -34,7 +34,7 @@ def count_caps(cap_list)
 
   cap_list.each_with_index do |c, i|
     if c.nil? || c.dir? != cap_list[start].dir?
-      range_list << CapRange.new(start, i-1, cap_list[start])
+      range_list << CapRange.new(start, i-1, cap_list[start].dir?)
 
       if cap_list[start].dir? == 1
         front += 1
@@ -52,4 +52,33 @@ def print_cap_list(cap_list)
     printf x&.dir?.to_s + ' '
   end
   puts ''
+end
+
+## MAIN
+caps = create_cap_list(10)
+puts "-- Initial CAPS --"
+print_cap_list(caps)
+cap_commands = count_caps(caps)
+command_mode = 0
+if cap_commands[:back] < cap_commands[:front]
+  command_mode = 1
+end
+puts ''
+
+cap_commands[:range].each do |c|
+  f, t, d = c.info[:from], c.info[:to], c.info[:dir]
+  if d == command_mode
+    if f == t
+      puts "-- Command: 'Flip! #{f}' --"
+    else
+      puts "-- Command: 'Flip! from #{f} to #{t}' --"
+    end
+    
+    caps.each_with_index do |x, i|
+      if i.between?(f, t)
+        x.flip!
+      end
+    end
+    print_cap_list(caps)
+  end
 end
