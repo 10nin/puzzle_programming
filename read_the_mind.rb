28ddd70e7ad70e6b0a30a9b3f0c89161ca9a4c56
Akira Ota 10nin <akira19870221@gmail.com>
2018-10-09 23:12:29 +0900

    Fixed: generate_deck method loop binding num->mark to mark->num.

--- read_the_mind.rb
+++ read_the_mind.rb
@@ -1,19 +1,21 @@
 def generate_deck
   mark = ['_S', '_H', '_D', '_C']
-  nums = (2..10).to_a
-  nums.push(*['J', 'Q', 'K', 'A'])
-  mark.collect do |m|
-    nums.map{ |n| n.to_s + m }
-  end
+  nums = ['A']
+  nums.push(*(2..10).to_a)
+  nums.push(*['J', 'Q', 'K'])
+  nums.collect do |n|
+    mark.map{ |m| n.to_s + m }
+  end.flatten
 end
 
-def assistantOrdersCards(deck)
+def assistant_orders_cards(deck)
   puts 'Cards are character strings as shown below.'
   puts "Ordering is: #{deck}"
   cards, cind, cardsuites, cnumbers = [], [], [], []
   numsuites = [0, 0, 0, 0]
-  (0..4).each do |i|
-    puts "Please give card #{i+1}"
+  (1..5).each do |i|
+    puts "Please give card #{i}"
   end
 end
-  
+
+assistant_orders_cards generate_deck
